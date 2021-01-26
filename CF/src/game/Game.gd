b089300d9fef11efdf00f140f30da49dc96612f8
Michael Redford <michaeliredford@gmail.com>
2021-01-26 12:26:27 +0800

    implemented game scene and global signals

---
+++ CF/src/game/Game.gd
@@ -0,0 +1,19 @@
+extends Control
+
+export var timeline = 'Intro'
+
+func _ready():
+	var new_dialog = Dialogic.start(timeline)
+	add_child(new_dialog)
+	new_dialog.connect("dialogic_signal", self, '_signal_from_dialogic')
+
+func _signal_from_dialogic(value):
+	if value=="change_background":
+		$Background._change_background()
+		
+	print("signal called for:", value)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
