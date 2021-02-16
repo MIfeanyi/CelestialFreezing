395f9f87ab42d7375d2210930a54328c8ff7d761
Michael Redford <michaeliredford@gmail.com>
2021-02-17 00:05:43 +0900

    added rounding function

--- CF/src/signals/global.gd
+++ CF/src/signals/global.gd
@@ -9,6 +9,10 @@ static func audio_stop(d):
 	d.FX.AudioStreamPlayer.stop()
 	pass
 
+static func round_place(num,places): #https://godotengine.org/qa/29110/how-to-round-to-a-specific-decimal-placehttps://godotengine.org/qa/29110/how-to-round-to-a-specific-decimal-place
+	return (round(num*pow(10,places))/pow(10,places))
+
+
 
 
 # Called when the node enters the scene tree for the first time.
